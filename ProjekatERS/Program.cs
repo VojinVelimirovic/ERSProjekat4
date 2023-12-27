ce340e03c2c6a07a4a07e5c8d0bd856c196a1bea
NikolaKrmpot <krmpotnikola@gmail.com>
2023-12-27 20:46:49 +0100

    Dodavanje HidroGeneratora

--- ProjekatERS/Program.cs
+++ ProjekatERS/Program.cs
@@ -8,13 +8,15 @@ namespace ProjekatERS
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //pokretanje programa
             Consumer.Consumer potrosac = new Consumer.Consumer();
             Generators.SolarniPanel solar1 = new Generators.SolarniPanel("solarni1");
             Generators.SolarniPanel solar2 = new Generators.SolarniPanel("solarni2");
             Generators.Vetrogenerator vetar1 = new Generators.Vetrogenerator("vetar1");
+            Generators.Vetrogenerator vetar2 = new Generators.Vetrogenerator("vetar2");
+            Generators.HidroGenerator hidroGenerator = new Generators.HidroGenerator("hidro");
             potrosac.Trosi();
         }
     }
